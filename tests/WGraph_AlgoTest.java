d090d6784e97b7f386802f8e381fb19c812a8a34
ziv-madar <58150490+ziv-madar@users.noreply.github.com>
2020-11-19 23:12:29 +0200

    Add files via upload

--- tests/WGraph_AlgoTest.java
+++ tests/WGraph_AlgoTest.java
@@ -6,13 +6,10 @@ import org.junit.jupiter.api.Test;
 
 class WGraph_AlgoTest {
 
-	
-
-	
 
 	@Test
 	void testCopy() {
-		
+
 		weighted_graph_algorithms wga = new WGraph_Algo();
 		weighted_graph ga = new WGraph_DS();
 		wga.init(ga);
@@ -24,12 +21,11 @@ class WGraph_AlgoTest {
 		assertEquals(ga.getV().toString(), ga1.getV().toString());
 		ga.addNode(5);
 		assertNotEquals(ga.getV().toString(), ga1.getV().toString());
-		
 	}
 
 	@Test
 	void testIsConnected() {
-		
+
 		weighted_graph_algorithms wga = new WGraph_Algo();
 		weighted_graph ga = new WGraph_DS();
 		wga.init(ga);
@@ -41,96 +37,82 @@ class WGraph_AlgoTest {
 		ga.connect(1, 2, 9);
 		ga.connect(2, 3, 1.3);
 		assertTrue(wga.isConnected());
-		
 		ga.removeEdge(1, 2);
 		assertFalse(wga.isConnected());
 		assertFalse(wga.isConnected());
-		
 	}
 
 	@Test
 	void testShortestPathDist() {
+
 		weighted_graph_algorithms wga = new WGraph_Algo();
 		WGraph_DS ga = new WGraph_DS();
 		wga.init(ga);
-		
 		ga.addNode(1);
 		ga.addNode(2);
 		ga.addNode(3);
 		ga.addNode(4);
 		ga.addNode(5);
-
 		ga.connect(1, 2, 15);
 		ga.connect(1, 3, 2);
 		ga.connect(1, 4, 17);
 		ga.connect(1, 5, 20);
 		ga.connect(3, 5, 4);
 		ga.connect(5, 4, 1);
-		
-		assertEquals(wga.shortestPathDist(1, 4), 7);
-		
+		assertEquals(wga.shortestPathDist(1, 4), 7);	
 	}
+
 
 	@Test
 	void testShortestPath() {
-		
+
 		weighted_graph_algorithms wga = new WGraph_Algo();
 		WGraph_DS ga = new WGraph_DS();
 		wga.init(ga);
-		
 		ga.addNode(1);
 		ga.addNode(2);
 		ga.addNode(3);
 		ga.addNode(4);
 		ga.addNode(5);
-
 		ga.connect(1, 2, 15);
 		ga.connect(1, 3, 2);
 		ga.connect(1, 4, 17);
 		ga.connect(1, 5, 20);
 		ga.connect(3, 5, 4);
 		ga.connect(5, 4, 1);
-		
 		assertEquals(wga.shortestPath(1, 4).toString(), "[NodeInfo [_key=1, _info=|2,15.0|3,2.0|4,17.0|5,20.0|, _tag=0.0], NodeInfo [_key=3, _info=|1,2.0|5,4.0|, _tag=0.0], NodeInfo [_key=5, _info=|1,20.0|3,4.0|4,1.0|, _tag=0.0], NodeInfo [_key=4, _info=|1,17.0|5,1.0|, _tag=0.0]]");
 	}
 
+
 	@Test
 	void testSave() {
-		
+
 		WGraph_DS g = new WGraph_DS();
 		g.addNode(1);
 		g.addNode(2);
 		g.addNode(3);
 		g.addNode(4);
 		g.addNode(5);
-
 		g.connect(1, 2, 15);
 		g.connect(1, 3, 2);
 		g.connect(1, 4, 17);
 		g.connect(1, 5, 20);
 		g.connect(3, 5, 4);
 		g.connect(5, 4, 1);
-
 		WGraph_DS g1 = new WGraph_DS();
 		g1.addNode(1);
 		g1.addNode(2);
 		g1.addNode(3);
-
 		weighted_graph_algorithms w = new WGraph_Algo();
 		weighted_graph_algorithms w2 = new WGraph_Algo();
-		
 		w2.init(g1);
 		assertEquals(w2.getGraph().getV().toString(), "[NodeInfo [_key=1, _info=, _tag=0.0], NodeInfo [_key=2, _info=, _tag=0.0], NodeInfo [_key=3, _info=, _tag=0.0]]");
 		w.init(g);
-		
 		w.save("f1.txt");
-		
 		w2.load("f1.txt");
 		assertEquals(w2.getGraph().getV().toString(), "[NodeInfo [_key=1, _info=|2,15.0|3,2.0|4,17.0|5,20.0|, _tag=0.0], NodeInfo [_key=2, _info=|1,15.0|, _tag=0.0], NodeInfo [_key=3, _info=|1,2.0|5,4.0|, _tag=0.0], NodeInfo [_key=4, _info=|1,17.0|5,1.0|, _tag=0.0], NodeInfo [_key=5, _info=|1,20.0|3,4.0|4,1.0|, _tag=0.0]]");
-		
-		
+
 	}
 
-	
 
 }
--- tests/WGraph_DSTest.java
+++ tests/WGraph_DSTest.java
@@ -6,13 +6,10 @@ import org.junit.jupiter.api.Test;
 
 class WGraph_DSTest {
 
-	@Test
-	void testWGraph_DS() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	void testGetNode() {
+
 		weighted_graph g = new WGraph_DS();
 		g.addNode(0);
 		g.addNode(1);
@@ -23,26 +20,24 @@ class WGraph_DSTest {
 		assertEquals(n.toString(),"NodeInfo [_key=0, _info=|1,5.0|2,7.0|, _tag=0.0]");
 		node_info n2 = g.getNode(3);
 		assertNull(n2);
-
 	}
+
 
 	@Test
 	void testHasEdge() {
+
 		weighted_graph g = new WGraph_DS();
 		g.addNode(400);
 		g.addNode(125);
 		g.addNode(8);
 		g.addNode(17);
-
 		g.connect(17, 125, 52.784);
 		g.connect(17, 50, 7000);
 		g.connect(17, 17, 0);
-
 		assertTrue(g.hasEdge(125, 17));
 		assertFalse(g.hasEdge(17, 50));
 		assertFalse(g.hasEdge(17, 8));
 		assertFalse(g.hasEdge(17, 17));
-
 
 	}
 
@@ -50,78 +45,62 @@ class WGraph_DSTest {
 	void testGetEdge() {
 
 		weighted_graph g = new WGraph_DS();
-
 		g.addNode(125);
 		g.addNode(8);
 		g.addNode(17);
-
 		g.connect(17, 125, 52.784);
 		g.connect(17, 50, 7000);
 		g.connect(17, 17, 0);
-
 		assertEquals(g.getEdge(17, 125), 52.784);
 		assertEquals(g.getEdge(17, 50), -1);
 		assertEquals(g.getEdge(17, 8), -1);
-
-
 	}
+
 
 	@Test
 	void testAddNode() {
 
 		weighted_graph g = new WGraph_DS();
-
 		g.addNode(125);
 		g.addNode(8);
 		g.addNode(170000);
-
 		g.connect(8, 170000, 45);
 		g.connect(170000, 125, 2);
-
 		assertEquals(g.getV().toString(), "[NodeInfo [_key=170000, _info=|8,45.0|125,2.0|, _tag=0.0], NodeInfo [_key=8, _info=|170000,45.0|, _tag=0.0], NodeInfo [_key=125, _info=|170000,2.0|, _tag=0.0]]");
 		g.addNode(8);
 		assertEquals(g.getV().toString(), "[NodeInfo [_key=170000, _info=|8,45.0|125,2.0|, _tag=0.0], NodeInfo [_key=8, _info=|170000,45.0|, _tag=0.0], NodeInfo [_key=125, _info=|170000,2.0|, _tag=0.0]]");
 
 	}
+
 
 	@Test
 	void testConnect() {
 
 		weighted_graph g = new WGraph_DS();
-
 		g.addNode(125);
 		g.addNode(8);
 		g.addNode(170000);
-
 		g.connect(8, 170000, 45);
 		g.connect(170000, 125, 2);
 		g.connect(8, 8, 7);
 		g.connect(50, 51, 9);
-
 		assertTrue(g.hasEdge(170000, 8));
 		assertFalse(g.hasEdge(8, 8));
 		assertFalse(g.hasEdge(50, 51));
 
-
-
-
 	}
+
 
 	@Test
 	void testGetV() {
 
 		weighted_graph g = new WGraph_DS();
-
 		assertEquals(g.getV().toString(), "[]");
-
 		g.addNode(125);
 		g.addNode(8);
 		g.addNode(170000);
-
 		g.connect(8, 170000, 45);
 		g.connect(170000, 125, 2);
-
-
 		assertEquals(g.getV().toString(), "[NodeInfo [_key=170000, _info=|8,45.0|125,2.0|, _tag=0.0], NodeInfo [_key=8, _info=|170000,45.0|, _tag=0.0], NodeInfo [_key=125, _info=|170000,2.0|, _tag=0.0]]");
 
 
@@ -132,94 +111,70 @@ class WGraph_DSTest {
 	void testGetVInt() {
 
 		weighted_graph g = new WGraph_DS();
-
 		g.addNode(125);
 		g.addNode(8);
 		g.addNode(5);
-
 		g.connect(8, 5, 45);
 		g.connect(5, 125, 2);
-
-
 		assertEquals(g.getV(5).toString(), "[NodeInfo [_key=8, _info=|5,45.0|, _tag=0.0], NodeInfo [_key=125, _info=|5,2.0|, _tag=0.0]]");
 		assertNull(g.getNode(59));
 	}
 
+
 	@Test
 	void testRemoveNode() {
-
 		weighted_graph g = new WGraph_DS();
-
 		g.addNode(125);
 		g.addNode(8);
 		g.addNode(5);
-
 		g.connect(8, 5, 45);
 		g.connect(5, 125, 2);
 		g.connect(8, 125, 0.25);
-
 		g.removeNode(5);
-
-
 		assertEquals(g.getNode(8).toString(), "NodeInfo [_key=8, _info=|125,0.25|, _tag=0.0]");
 		assertEquals(g.getNode(125).toString(), "NodeInfo [_key=125, _info=|8,0.25|, _tag=0.0]");
 
-
 	}
+
 
 	@Test
 	void testRemoveEdge() {
 
 		weighted_graph g = new WGraph_DS();
-
 		g.addNode(125);
 		g.addNode(8);
 		g.addNode(5);
-
 		g.connect(8, 5, 45);
 		g.connect(5, 125, 2);
 		g.connect(8, 125, 0.25);
-
 		g.removeEdge(8, 5);
 		g.removeEdge(8, 5);
 		g.removeEdge(8, 6);
-
 		assertEquals(g.getV(5).toString(), "[NodeInfo [_key=125, _info=|5,2.0|8,0.25|, _tag=0.0]]");
 		assertEquals(g.getV(8).toString(), "[NodeInfo [_key=125, _info=|5,2.0|8,0.25|, _tag=0.0]]");
 
-
 	}
 
-	@Test
-	void testNodeSize() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	void testEdgeSize() {
-		weighted_graph g = new WGraph_DS();
 
+		weighted_graph g = new WGraph_DS();
 		g.addNode(125);
 		g.addNode(8);
 		g.addNode(5);
 		g.addNode(38);
 		g.addNode(45);
-		
-
 		g.connect(8, 5, 45);
 		g.connect(5, 125, 2);
 		g.connect(8, 125, 0.25);
 		g.connect(45, 5, 4);
 		g.connect(38, 5, 3);
-
 		g.removeEdge(8, 5);
 		g.removeEdge(8, 5);
 		g.removeEdge(8, 6);
 		g.removeNode(5);
-		
 		assertEquals(g.edgeSize(), 1);
 	}
-
-
 
 }
